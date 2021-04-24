534b6e10a28daf6a076ea222007d84c372344076
Rafi0221 <rafal.pyzik@student.uj.edu.pl>
2021-04-24 22:46:25 +0200

    added images to AbstractNode

---
+++ src/code/controls/AbstractNode.java
@@ -0,0 +1,37 @@
+package code.controls;
+
+import javafx.scene.Cursor;
+import javafx.scene.layout.AnchorPane;
+
+import java.awt.image.BufferedImage;
+import java.nio.Buffer;
+
+public abstract class AbstractNode extends AnchorPane {
+
+    Canvas canvas;
+
+    double relativeX;
+    double relativeY;
+
+    BufferedImage out1;
+    BufferedImage out2;
+    BufferedImage out3;
+
+    //some filter type
+
+    public AbstractNode(Canvas canvas){
+        super();
+        this.canvas=canvas;
+        this.setOnMousePressed(me -> {
+            this.relativeX = this.getLayoutX() - me.getSceneX();
+            this.relativeY = this.getLayoutY() - me.getSceneY();
+            this.setCursor(Cursor.MOVE);
+        });
+        this.setOnMouseDragged( me -> {
+            this.setLayoutX(me.getSceneX()+relativeX);
+            this.setLayoutY(me.getSceneY()+relativeY);
+        });
+        this.setOnMouseEntered( me -> this.setCursor(Cursor.HAND));
+        this.setOnMouseReleased( me -> this.setCursor(Cursor.HAND));
+    }
+}
