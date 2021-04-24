55b9a282845ee148f7924cb2cafe39cfdb3b81ec
Rafi0221 <rafal.pyzik@student.uj.edu.pl>
2021-04-24 23:31:23 +0200

    added filer choosing in NodeControl

--- src/code/controls/NodeControl.java
+++ src/code/controls/NodeControl.java
@@ -1,10 +1,15 @@
 package code.controls;
 
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 
@@ -26,6 +31,8 @@ public class NodeControl extends AbstractNode {
     Socket s6;
     @FXML
     Socket s7;
+    @FXML
+    Pane topPane;
 
     public NodeControl(Canvas canvas) {
         super(canvas);
@@ -35,5 +42,19 @@ public class NodeControl extends AbstractNode {
         try { fxmlLoader.load(); } catch (IOException exception) { throw new RuntimeException(exception); }
         s1.node=this; s2.node=this; s3.node=this; s4.node=this;
         s5.node=this; s6.node=this; s7.node=this;
+
+        String listOfFilters[]={"filter1","filter2","filter3"};
+        ComboBox comboBox= new ComboBox(FXCollections.observableArrayList(listOfFilters));
+        comboBox.setOnAction(
+                new EventHandler<ActionEvent>(){
+                    @Override
+                    public void handle(final ActionEvent e){
+                        //TODO
+                        //set filter to appropriate type
+                        //comboBox.getValue()
+                    }
+                }
+        );
+        topPane.getChildren().add(comboBox);
     }
 }
