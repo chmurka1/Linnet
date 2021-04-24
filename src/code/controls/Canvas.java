e10ed3f9446d10f0d657bf23e65508b09377ef72
Rafi0221 <rafal.pyzik@student.uj.edu.pl>
2021-04-24 16:36:32 +0200

    canvas contextMenu adds nodes

--- src/code/controls/Canvas.java
+++ src/code/controls/Canvas.java
@@ -1,11 +1,16 @@
 package code.controls;
 
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 
@@ -16,9 +21,7 @@ public class Canvas extends ScrollPane {
     @FXML
     NodeControl node2;
     @FXML
-    InputNode inputNode1;
-    @FXML
-    OutputNode outputNode1;
+    Pane pane;
 
     public Canvas() {
         super();
@@ -30,10 +33,51 @@ public class Canvas extends ScrollPane {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+
         ContextMenu contextMenu = new ContextMenu();
-        contextMenu.getItems().addAll(new MenuItem("1"),new MenuItem("1"),new MenuItem("1"));
+        MenuItem menuItem1 = new MenuItem("add input node");
+        menuItem1.setOnAction(
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        addInputNode();
+                    }
+                }
+        );
+        MenuItem menuItem2 = new MenuItem("add node control");
+        menuItem2.setOnAction(
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        addNodeControl();
+                    }
+                }
+        );
+        MenuItem menuItem3 = new MenuItem("add output node");
+        menuItem3.setOnAction(
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        addOutputNode();
+                    }
+                }
+        );
+        contextMenu.getItems().addAll(menuItem1,menuItem2,menuItem3);
         this.setContextMenu(contextMenu);
+
         this.addLink(node1,node2,node1.s7,node2.s2);
+    }
+
+    public void addInputNode(){
+        pane.getChildren().add(new InputNode());
+    }
+
+    public void addOutputNode(){
+        pane.getChildren().add(new OutputNode());
+    }
+
+    public void addNodeControl(){
+        pane.getChildren().add(new NodeControl());
     }
 
     public void removeLink(Link link) {
